853329c30a5390fdf250887860b2e4deb0a719bb
Alexander Stekolshchikov <alexstekk@yandex.ru>
2024-10-03 15:13:19 +0300

    basic logic with redux

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,9 @@ const initialState = {
   cardList: [],
   turns: 0,
   firstCard: null,
-  seconsCard: null,
+  secondCard: null,
   disabledCards: false,
+  score: 0,
 };
 
 const gameSlice = createSlice({
@@ -15,13 +16,32 @@ const gameSlice = createSlice({
     setCards: (state, action) => {
       state.cardList = action.payload;
     },
+    markMatchedCards: (state, action) => {
+      state.cardList = state.cardList.map((card) => (card.src === action.payload ? { ...card, isMatched: true } : card));
+    },
+    setFirstCard: (state, action) => {
+      state.firstCard = action.payload;
+    },
+    setSecondCard: (state, action) => {
+      state.secondCard = action.payload;
+    },
+    resetTurns: (state, action) => {
+      state.turns = 0;
+    },
+    increaseTurns: (state, action) => {
+      state.turns = state.turns + 1;
+    },
+    disableCards: (state, action) => {
+      state.disabledCards = action.payload;
+    },
+    increaseScore: (state, action) => {
+      state.score = state.score + 1;
+    },
   },
 });
 
-export const { setCards } = gameSlice.actions;
+export const { setCards, setFirstCard, setSecondCard, setTurns, disableCards, increaseTurns, resetTurn, increaseScore, resetTurns,markMatchedCards } = gameSlice.actions;
 
 export const store = configureStore({
-  reducer: {
-    game: gameSlice.reducer,
-  },
+  reducer: gameSlice.reducer,
 });
